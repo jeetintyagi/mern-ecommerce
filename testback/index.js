e7f48d17d5a290155cb5a33763843394e80466f3
jeetintyagi <jt9999709701@gmail.com>
2020-04-01 14:45:02 +0530

    nodemon added and index.js edited

--- testback/index.js
+++ testback/index.js
@@ -1,2 +1,18 @@
-const express =require("express");
-const app =express();
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+        return res.send('hello there');
+});
+
+app.get('/login', (req, res) => {
+        return res.send('You are visiting a login route');
+});
+app.get('/signup', (req, res) => {
+        return res.send('You are visiting a signup route');
+});
+
+app.listen(port, () => {
+        console.log("Server is up and running...")
+});
