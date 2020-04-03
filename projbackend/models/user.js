23d3a7849c3b045edcdb513673e9d258d4c2aee6
jeetintyagi <jt9999709701@gmail.com>
2020-04-03 15:53:34 +0530

    models/user.js added with userSchema

---
+++ projbackend/models/user.js
@@ -0,0 +1,72 @@
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+import { v1 as uuidv1 } from 'uuid';
+var userSchema = new mongoose.Schema({
+        name: {
+                type: String,
+                maxlength: 32,
+                required: true,
+                trim: true
+        },
+        lastName: {
+                type: String,
+                maxlength: 32,
+                trim: true
+        },
+        email: {
+                type: String,
+                trim: true,
+                required: true,
+                unique: true,
+        },
+        userinfo: {
+                type: String,
+                trim: true,
+        },
+        encry_password: {
+                type: String,
+                required: true,
+        },
+        salt: String,
+        role: {
+                type: Number,
+                default: 0,
+        },
+        purchases: {
+                type: Array,
+                default: [],
+        }
+})
+
+userSchema.virtual()
+        .set(function () {
+                this._password = password;
+                this.salt = uuidv1();
+                this.encry_password = ths.securePassword(password);
+
+        })
+        .get(function () {
+                return this._password;
+        })
+
+userSchema.method = {
+        authenticate: function (plainpassword) {
+                return this.securePassword(plainpassword) === this.encry_password;
+
+        },
+
+        securePassword: function (plainpassword) {
+                if (!password)
+                        return "";
+
+                try {
+                        return crypto.createHmac('sha256', this.salt)
+                                .update(plainpassword)
+                                .digest('hex');
+                } catch (err) {
+                        return "";
+                }
+        }
+
+}
+module.exports = mongoose.model("User", userSchema);
